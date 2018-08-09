427ac0f0975291731ed783eeac60d486ee2e5c08
kim hyun ki <saykim0727@naver.com>
2018-08-09 05:07:29 -0700

    test

---
+++ script/build_eos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#Build eos release/1.1.x & install
+
+  EOS_DIR=`basename "$PWD"`
+  if [ "$EOS_DIR" == "script" ]; 
+    then EOS_DIR="$PWD/../eos"	   
+  elif [ "$EOS_DIR" == "fuzz2wasm"];
+		then EOS_DIR="$PWD/eos"
+  else
+    printf "\\n\\tPlease cd into directory fuzz2wasm to run this script.\\n \\tExiting now.\\n\\n"
+		exit 1
+  fi
+  
+  `cd $EOS_DIR`
+  version=$1
+  if [ "$version" == "1.0" ];
+		then git checkout release/1.0.x
+  else 
+		git checkout release/1.1.x
+  fi
+
+  git pull
+	git submodule update --init --recursive
+  $EOS_DIR/eosio_build.sh
