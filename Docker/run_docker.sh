cb835844610767d8c4993733aa75f69bb00a7bff
k1rh4 <k1rh4.lee@gmail.com>
2018-09-04 15:57:41 +0900

    Add core patter

--- Docker/run_docker.sh
+++ Docker/run_docker.sh
@@ -11,7 +11,7 @@ sudo docker build --tag $NAME:1.0 ./
 
 PORT="-p 20000:20000"
 SHARED="-v `pwd`/tmp/:/tmp/" 
-OPTION="--rm  --cap-add=SYS_PTRACE --ulimit core=-1 --security-opt seccomp=unconfined"
+OPTION="--rm --privileged --cap-add=SYS_PTRACE --ulimit core=-1 --security-opt seccomp=unconfined"
 #OPTION=" --read-only --rm -v `pwd`/tmp/:/tmp"
 
 sudo docker run -idt --name $NAME $PORT $SHARED $OPTION $NAME:1.0 /bin/bash
