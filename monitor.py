0b6b71536dec7aaeab5e056d8245c856b9a59401
k1rh4 <k1rh4.lee@gmail.com>
2018-09-05 13:56:18 +0900

    Crash dump monitor

--- monitor.py
+++ monitor.py
@@ -21,10 +21,13 @@ class Monitor:
                 import psutil
 		import time
 		if psutil.pid_exists(pid) == False:
-			for filename in  os.listdir(self._core_dir):
-				if filename.find("core") >= 0 :
+			for filename in  os.listdir(self._core_dir)
+				if filename.find("core") and (filename.split(".")[-1].find("6") < 0) :
 					timer = time.time()
 					os.mkdir("%s/%s" % (this._crash_dir, timer))
 					move("%s/%s" % (this._core_dir,filename), "%s/%s" % (this._crash_dir, timer))
 					move("%s" % (self._contract),"%s/%s" % (this._crash_dir,timer))
 					print "[!] CORE & CRASH data is moved"
+                else:
+                    print "[!] Process still alive"
+
