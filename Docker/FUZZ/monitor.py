b8ad3626b369dc1cfe4cf2f76ddd2f10dd551d72
k1rh4 <k1rh4.lee@gmail.com>
2018-09-14 17:14:59 +0900

    Apply configParsor in monitor

--- Docker/FUZZ/monitor.py
+++ Docker/FUZZ/monitor.py
@@ -1,13 +1,14 @@
 import os
 import shutil
-
+from ConfigParsor import ConfigParsor
 class Monitor:
 	def __init__(self, core_dir = "/tmp/core" , crash_dir = "/tmp/crash" ):
 		self._core_dir   = core_dir
 		self._crash_dir = crash_dir 
 		with open("/FUZZ/config.ini","r") as f:
 			datalist = f.readlines()
-			self._contract = (datalist[4].split("="))[1][1:-2]
+                        self._contract = ConfigParsor("CONTRACT",datalist)
+			#self._contract = (datalist[4].split("="))[1][1:-2]
 
 		if os.path.isdir(core_dir) != True:
 			os.mkdir(core_dir)
