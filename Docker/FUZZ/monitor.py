741658145644951aa444ec8f8af18fecc82b3b54
saykim0727 <saykim0727@kaist.ac.kr>
2018-09-16 03:32:56 -0700

    change the var name

--- Docker/FUZZ/monitor.py
+++ Docker/FUZZ/monitor.py
@@ -7,7 +7,7 @@ class Monitor:
 		self._crash_dir = crash_dir 
 		with open("/FUZZ/config.ini","r") as f:
 			datalist = f.readlines()
-                        self._contract = ConfigParsor("TESTCASE",datalist)
+                        self._testcase = ConfigParsor("TESTCASE",datalist)
 			
 		if os.path.isdir(core_dir) != True:
 			os.mkdir(core_dir)
@@ -26,8 +26,8 @@ class Monitor:
 				timer = tiemr + 0.5
 			os.mkdir("%s/%s" % (self._crash_dir, timer))
 			move("%s/%s" % (self._core_dir,filename), "%s/%s" % (self._crash_dir, timer))
-			shutil.copytree("%s" % (self._contract),"%s/%s/testcasse" % (self._crash_dir,timer))
+			shutil.copytree("%s" % (self._testcase),"%s/%s/testcasse" % (self._crash_dir,timer))
 			return True
-		print self._contract, self._crash_dir
+		print self._testcase, self._crash_dir
 		return False
 
