e1ad39b55de3f475a5fdb542867bfa747d7e5ef4
saykim0727 <saykim0727@kaist.ac.kr>
2018-09-17 00:08:24 -0700

    upload crash

---
+++ Docker/CRASH/1537167771.61/testcasse/configParsor.py
@@ -0,0 +1,10 @@
+def ConfigParsor( Key, List ):
+	value = ""
+	for data in List :
+		if Key in data : 
+			sData = data.split("=")
+			value = sData[1].rstrip().strip("\"")
+			return value;
+	print "[E] There is no data for [%s] " % (Key)
+	return -1;
+
---
+++ Docker/CRASH/1537167771.61/testcasse/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pkill -9 python
+pkill -9 nodeos
+pkill -9 keosd
+
+rm -rf ~/eosio-wallet/*
+
+/usr/bin/python /FUZZ/fuzz.py 2>/dev/null
+
