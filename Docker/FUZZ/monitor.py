2edc2e1bbfa3757d5d6e39e7d5922d87fe13a546
saykim0727 <saykim0727@kaist.ac.kr>
2018-09-17 08:50:23 -0700

    bug fix

--- Docker/FUZZ/monitor.py
+++ Docker/FUZZ/monitor.py
@@ -27,8 +27,8 @@ class Monitor:
 				timer = tiemr + 0.5
 			os.mkdir("%s/%s" % (self._crash_dir, timer))
 			move("%s/%s" % (self._core_dir,filename), "%s/%s" % (self._crash_dir, timer))
+			move("%s" % ("/var/log/cron.log"), "%s/%s" % (self._crash_dir, timer))
 			shutil.copytree("%s" % (self._testcase + self._seed+"/"),"%s/%s/testcase" % (self._crash_dir,timer))
-			shutil.copytree("%s" % ("/var/log/cron.log"),"%s/%s/testcase" %(self._crash_dir,timer))
 			return True
 		print self._testcase, self._crash_dir
 		return False
