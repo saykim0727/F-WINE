bf69abb4dd86fee9b08744119c6ba8d5ef5a0275
k1rh4 <k1rh4.lee@gmail.com>
2018-10-18 16:40:21 +0900

    SendMail when crash detected

---
+++ ALRAM/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nohup /usr/bin/python ./sendmail.py &
+exit
+
---
+++ ALRAM/sendmail.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.mime.text import MIMEText
+
+import glob
+import time 
+
+# First you have to make app password in https://myaccount.google.com/security
+
+def sendMail(me, you, msg):
+    smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp.login(me, 'gdzwzenfuitnlizz')
+    msg = MIMEText(msg)
+    msg['Subject'] = 'TEST'
+    smtp.sendmail(me, you, msg.as_string())
+    smtp.quit()
+
+def main():
+    ALRAM_FLAG = 0
+    before_count = 0
+    after_count = 0
+    while 1 :
+        time.sleep(10);
+        fileList = glob.glob("../Docker/CRASH/*")
+        after_count =  len(fileList)
+        if(after_count > before_count):
+            before_count = after_count
+            sendMail('k1rh4.lee@gmail.com', 'k1rh4.lee@gmail.com', "\n".join(fileList))
+
+if __name__ =="__main__":
+    main()
