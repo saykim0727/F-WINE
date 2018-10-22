fc2f73fa12f6b2f1ca94e22a39ce20a92dcec865
saykim0727 <saykim0727@kaist.ac.kr>
2018-10-21 20:34:33 -0700

    remove crash dir

--- Docker/CRASH/1537112185.32/testcasse/hello/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/k1rh4/GIT/eos/contracts/hello
-# Build directory: /home/k1rh4/GIT/eos/build/contracts/hello
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(validate_hello_abi "/home/k1rh4/GIT/eos/build/scripts/abi_is_json.py" "/home/k1rh4/GIT/eos/contracts/hello/hello.abi")
--- Docker/CRASH/1537112185.32/testcasse/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-pkill -9 python
-pkill -9 nodeos
-pkill -9 keosd
-
-rm -rf ~/eosio-wallet/*
-
-/usr/bin/python /FUZZ/fuzz.py 2>/dev/null
-
