fbdc455d28a4e98c81dae24f5993ba3ea3d907f3
saykim0727 <saykim0727@naver.com>
2018-11-19 05:33:29 -0800

    rm dir

--- Crowler/eosbettransf/eosbettransf.wast
+++
@@ -1,9 +0,0 @@
-(module
-  (type $0 (func (param i64 i64 i64)))
-  (export "apply" (func $1))
-  
-  (func $1
-    (param $0 i64)
-    (param $1 i64)
-    (param $2 i64)
-    ))
--- Crowler/remover.sh
+++ Crowler/remover.sh
@@ -2,4 +2,4 @@
 
 
 echo $GREP
-rm -rf `ls |grep -v 'AllOfSite' |grep -v 'getDapp.py' | grep -v 'remover.sh'| grep -v 'bpList'`
+rm -rf `ls |grep -v 'AllOfSite' |grep -v 'getDapp.py' | grep -v 'remover.sh'| grep -v 'bpList' | grep -v 'getAbi.py'|grep -v 'getUser.py' | grep -v 'user.txt'|grep -v 'Dis*'`
