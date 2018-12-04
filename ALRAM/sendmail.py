e21f263a90a6e669a071214f356448594a3321dd
k1rh4 <k1rh4.lee@gmail.com>
2018-12-04 15:17:07 +0900

    SendMail with crash info

--- ALRAM/sendmail.py
+++ ALRAM/sendmail.py
@@ -5,7 +5,8 @@ import glob
 import time 
 
 # First you have to make app password in https://myaccount.google.com/security
-
+ANALYZER = "../CrashAnalyzer/"
+RUN_ANALY= "./run_docker.sh" 
 def sendMail(me, you, msg):
     smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     if me=="k1rh4.lee@gmail.com":
@@ -27,8 +28,21 @@ def main():
         after_count =  len(fileList)
         if(after_count > before_count):
             before_count = after_count
-            sendMail('k1rh4.lee@gmail.com', 'k1rh4.lee@gmail.com', "\n".join(fileList))
-            sendMail('saykim0727@ajou.ac.kr', 'saykim0727@ajou.ac.kr', "\n".join(fileList))
+            import os
+            recent_file = fileList[-1]
+            print "[D] recent_file : %s " %(recent_file.split("/")[-1])
+            os.chdir(ANALYZER)
+            cmd = RUN_ANALY + " " + recent_file.split("/")[-1]
+            print cmd 
+            os.system(cmd)
+            readData =""
+            if os.path.exists("./CRASH/log_crash.log"):
+                print "[+]log file exists" 
+                with open("./CRASH/log_crash.log") as f : readData = f.read()
+
+            contents = "\n".join(fileList)+"\n" + readData 
+            sendMail('k1rh4.lee@gmail.com', 'k1rh4.lee@gmail.com', contents)
+            sendMail('saykim0727@ajou.ac.kr', 'saykim0727@ajou.ac.kr', contents)
 
 if __name__ =="__main__":
     main()
