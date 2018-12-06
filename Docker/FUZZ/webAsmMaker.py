91ea674e91360e328c99a50611cc5e02dec9a584
saykim0727 <saykim0727@naver.com>
2018-12-05 22:48:58 -0800

    save webAsmMaker

---
+++ Docker/FUZZ/webAsmMaker.py
@@ -0,0 +1,16 @@
+#first insert our function, and insert fuction call
+#make a function instruction based on dictionary
+#====================================================
+
+class WebAsmMaker:
+    def __init__ (self):
+       self.dict = "/FUZZ/dictionary"
+
+    def setLocalVarNum(self,count):
+        pass
+    
+    def setInstruction(self,count):
+        pass
+
+
+
