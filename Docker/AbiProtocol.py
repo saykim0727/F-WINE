74dbfa476784c9aa7b23bf2f36a93f8defe70da9
k1rh4 <k1rh4@k1rh4ui-MacBook-Pro.local>
2018-12-07 15:20:59 +0900

    Finish structure

--- Docker/AbiProtocol.py
+++ Docker/AbiProtocol.py
@@ -3,12 +3,11 @@ import glob
 class abiCook:
     def __init__(self, abiPath="betdicegroup/betdicegroup.abi__"):
         self.filePath = "../Docker/SEED/" + abiPath
-        self.base       = 30
+        self.base       = 0
         self.byteSize   = 2
         tmp = glob.glob(self.filePath)
-        if len(tmp) : fileName = tmp[0]
-        import os
-        if os.path.exists(fileName) :
+        if len(tmp) :
+            fileName = tmp[0]
             with open(fileName,"rb") as f :
                 dataByte = f.read()
                 self.abiByte=dataByte.encode("hex")
@@ -26,38 +25,32 @@ class abiCook:
         self.base   = self.base + self.byteSize
         return cnt
 
+    def getDic(self, v1=[]):
+        tmpDic  = {}
+        for key in v1: tmpDic[key] = self.carving()
+        return tmpDic
+
     def cook(self):
-        fileFormat = {"version":{},"types":[],"structs":[]}
-        fileFormat["version"] = self.abiByte[0:30].decode("hex")
+        fileFormat = {"version":"","structs":[],"types":[],"actions":[],"tables":[]}
+        fileFormat["version"] = self.carving()
 
         ## set types elements
         tmpList = []
         for i in range(self.getCount("types")):
-            tmpDic                  = {}
-            tmpDic["new_type_name"] = self.carving();
-            tmpDic["type"]          = self.carving();
-            tmpList.append(tmpDic)
+            tmpList.append(self.getDic(["new_type_name","type"]))
         fileFormat["types"] = tmpList
 
         #set structure elements
         tmpList = []
         for i in range(self.getCount("structure")):
-            tmpDic          = {}
-            tmpDic["name"]  = self.carving()
-            tmpDic["base"]  = self.carving()
-
+            tmpDic = self.getDic(["name","base"])
             tmpList2    = []
             for j in range(self.getCount("fields")):
-                tmpDic2         = {}
-                tmpDic2["name"] = self.carving()
-                tmpDic2["type"] = self.carving()
-                tmpList2.append(tmpDic2)
-
+                tmpList2.append(self.getDic(["name","type"]))
             tmpDic["fields"] = tmpList2
             tmpList.append(tmpDic)
         fileFormat["structs"] = tmpList
-
-        print fileFormat
+        #print fileFormat
 
         #set action elements 
         for i in range(self.getCount("action")):
@@ -72,48 +65,45 @@ class abiCook:
             print self.abiByte[self.base : self.base+cnt]
             self.base = self.base+cnt
 
-        iteratorNum = int( self.abiByte[self.base:self.base+self.byteSize],16)
-        print iteratorNum
-        self.base = self.base + self.byteSize
-        self.base = self.base + 16
-        
-        print self.abiByte[self.base::]
 
-       #for i in range(0,iteratorNum):
-        for i in range(0,2):
+        #set Table elements 
 
-
-            print "[T] " , self.abiByte[self.base:self.base+16],
-            print "->",
-            cnt = int (self.abiByte[self.base:self.base+ self.byteSize],16)
+        iteratorNum = self.getCount("Table")
+        #for i in range(0,iteratorNum):
+        for i in range(0,iteratorNum):
+            print "[??] "+ self.abiByte[self.base:self.base+16]
+            self.base = self.base + 16
+            print "[T] " ,
+            cnt = self.getCount("T Str")
             cnt = (cnt) * self.byteSize
-            self.base = self.base + self.byteSize
             print self.abiByte[self.base:self.base+cnt].decode("hex"),
-            print ":",
+            print "->",
             print self.abiByte[self.base: self.base+cnt]
             self.base = self.base + cnt
             i3 = 0
-            listCnt = int(self.abiByte[self.base:self.base+self.byteSize],16)
-            self.base = self.base + self.byteSize
-            for i2 in range(0,listCnt):
-                length = int(self.abiByte[self.base:self.base+self.byteSize],16)
-                self.base = self.base + self.byteSize
-                print "[T2] ",
+            listCnt = self.getCount("T1")
+            for i2 in range(listCnt):
+                length = self.getCount("T2 Str")
+                print "[T2]",
                 print self.abiByte[self.base:self.base+length*2].decode("hex"),
                 print "->",
                 print self.abiByte[self.base:self.base+length*2]
                 self.base = self.base + length*2
-
-            listCnt = int(self.abiByte[self.base:self.base+self.byteSize],16)
-            self.base = self.base + self.byteSize
-            for i2 in range(0,listCnt):
-                length = int(self.abiByte[self.base:self.base+self.byteSize],16)
-                self.base = self.base + self.byteSize
-                print "[T2] ",
+            listCnt = self.getCount("T2")
+            for i2 in range(listCnt):
+                length = self.getCount("T2 Str")
+                print "[T2]",
                 print self.abiByte[self.base:self.base+length*2].decode("hex"),
                 print "->",
                 print self.abiByte[self.base:self.base+length*2]
-             
+                self.base = self.base+length*2
+            
+            length  = self.getCount("T3")
+            print "[T3]",
+            print self.abiByte[self.base:self.base+length*2].decode("hex"),
+            print "->",
+            print self.abiByte[self.base:self.base + length*2]
+            self.base = self.base + length*2
 
 
     print "#######################################################"
